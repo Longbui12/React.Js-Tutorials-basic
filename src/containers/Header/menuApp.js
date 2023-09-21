aeee6dea81e06e351cc63eb36a7121371cb4a39c
rn25-long-app <bhlong3112@gmail.com>
2023-09-21 17:36:09 +0700

    create route & create file  for Manage Schedule of doctor

--- src/containers/Header/menuApp.js
+++ src/containers/Header/menuApp.js
@@ -25,9 +25,15 @@ export const adminMenu = [
         //   },
         // ],
       },
+      // {
+      //   name: "menu.admin.manage-admin",
+      //   link: "/system/user-admin",
+      // },
+
       {
-        name: "menu.admin.manage-admin",
-        link: "/system/user-admin",
+        // quản lý kế hoạch khám bệnh của bác sỹ
+        name: "menu.doctor.manage-schedule",
+        link: "/doctor/manage-schedule",
       },
     ],
   },
@@ -58,6 +64,19 @@ export const adminMenu = [
       {
         name: "menu.admin.manage-handbook",
         link: "/system/manage-handbook",
+      },
+    ],
+  },
+];
+
+export const doctorMenu = [
+  {
+    name: "menu.admin.manage-user",
+    menus: [
+      {
+        // quản lý kế hoạch khám bệnh của bác sỹ
+        name: "menu.doctor.manage-schedule",
+        link: "/doctor/manage-schedule",
       },
     ],
   },
