d66871007f2367a1c9173b950e6686e0d1ed8e79
rn25-long-app <bhlong3112@gmail.com>
2023-09-29 05:48:28 +0700

    Complete the Select Medical Examination Schedule Component

--- src/containers/System/Doctor/ManageSchedule.js
+++ src/containers/System/Doctor/ManageSchedule.js
@@ -138,13 +138,18 @@ class ManageSchedule extends Component {
       doctorId: selectedDoctor.value,
       formatedDate: formatedDate,
     });
-    console.log("check res :", res);
-    console.log("check result :", result);
+    if (res && res.errCode === 0) {
+      toast.success("Save infor succesed !");
+    } else {
+      toast.error("error saveBulkScheduleDoctor !");
+      console.log("error saveBulkScheduleDoctor >>> res :", res);
+    }
   };
   render() {
     let { rangeTime } = this.state;
     let { language } = this.props;
-    // console.log("check rangeTime in render :", rangeTime);
+    let yesterday = new Date(new Date().setDate(new Date().getDate() - 1));
+
     return (
       <div className="manage-schedule-container">
         <div className="m-s-title">
@@ -170,7 +175,7 @@ class ManageSchedule extends Component {
                 onChange={this.handleOnchangeDatePicker}
                 className="form-control"
                 value={this.state.currentDate}
-                minDate={new Date()}
+                minDate={yesterday}
               />
             </div>
             <div className="col-12 pick-hour-container">
