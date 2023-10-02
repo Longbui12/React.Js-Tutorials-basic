0111692b38883565bd6aa730dba20a954dd805b6
rn25-long-app <bhlong3112@gmail.com>
2023-10-03 05:56:46 +0700

    translation for vi & en of file manage-doctor , save more information for file ManageDoctor.js & write redux

--- src/store/reducers/adminReducer.js
+++ src/store/reducers/adminReducer.js
@@ -11,6 +11,8 @@ const initialState = {
   topDoctors: [],
   allDoctors: [],
   allScheduleTime: [],
+
+  allRequiredDoctorInfor: [],
 };
 
 const adminReducer = (state = initialState, action) => {
@@ -123,6 +125,17 @@ const adminReducer = (state = initialState, action) => {
       };
     case actionTypes.FETCH_ALLCODE_SCHEDULE_TIME_FAILED:
       state.allScheduleTime = [];
+      return {
+        ...state,
+      };
+
+    case actionTypes.FETCH_REQUIRED_DOCTOR_INFOR_SUCCESS:
+      state.allRequiredDoctorInfor = action.data;
+      return {
+        ...state,
+      };
+    case actionTypes.FETCH_REQUIRED_DOCTOR_INFOR_FAILED:
+      state.allRequiredDoctorInfor = [];
       return {
         ...state,
       };
