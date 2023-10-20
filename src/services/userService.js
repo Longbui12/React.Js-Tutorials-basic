c76664f2973351d534e1497bb0ff582b2a0848db
rn25-long-app <bhlong3112@gmail.com>
2023-10-20 11:02:22 +0700

    edit field date & add birthday in function handleConfirmBooking of file BookingModal.js ; write API getAllPatientForDoctor , import getAllPatientForDoctor & handle functions in file ManagePatient.js ; add attribuite css for file ManagePatient.scss

--- src/services/userService.js
+++ src/services/userService.js
@@ -101,6 +101,11 @@ const getAllDetailClinicById = (data) => {
   return axios.get(`/api/get-detail-clinic-by-id?id=${data.id}`);
 };
 
+const getAllPatientForDoctor = (data) => {
+  return axios.get(
+    `/api/get-list-patient-for-doctor?doctorId=${data.doctorId}&date=${data.date}`
+  );
+};
 export {
   handleLoginApi,
   getAllUsers,
@@ -124,4 +129,5 @@ export {
   createNewClinic,
   getAllClinic,
   getAllDetailClinicById,
+  getAllPatientForDoctor,
 };
